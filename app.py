1512078c97f4a23dbbb9716b64bb24017c04c649
BenMorris <bentechmorris@gmail.com>
2020-10-12 13:10:09 -0700

    Commiting to Dev Branch

--- app.py
+++ app.py
@@ -10,3 +10,16 @@ app.layout = html.Div()
 #Run the app
 if __name__ == '_main_':
     app.run.server(debug=True)
+
+
+app.layout = html.Div(children=[
+                    html.Div(className='row',
+                             children=[
+                                 html.Div(className='four columns div-user-controls',
+                                     children = [
+    html.H2('Dash - STOCK PRICES'),
+    html.P('''Visualising time series with Plotly - Dash'''),
+    html.P('''Pick one or more stocks from the dropdown below.''')
+],
+                                 html.Div(className='eight columns div-for-charts bg-grey')
+                             )
